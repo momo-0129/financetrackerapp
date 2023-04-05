51f2eb4662b1c1ad8eda093a735ae2d3160eeb9b
momo-0129 <tonyfang8733@gmail.com>
2023-04-05 14:38:57 +0900

    Add turbo stream to render search result

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,8 +2,12 @@ class StocksController < ApplicationController
   def search
     if params[:stock].present? 
       @stock = Stock.new_lookup(params[:stock])
-      if @stock   
-      render 'users/my_portfolio'
+      if @stock  
+        respond_to do |format|
+          format.turbo_stream {
+            render turbo_stream: turbo_stream.append("test", partial: "users/result")
+          }
+        end
       else  
         flash[:alert] = "Please enter a valid symbol to search"
         redirect_to my_portfolio_path
