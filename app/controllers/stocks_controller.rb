1b845529c67d62f227b24854712276f3b30c850d
momo-0129 <tonyfang8733@gmail.com>
2023-04-05 16:16:57 +0900

    fix flash messages

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,17 +5,24 @@ class StocksController < ApplicationController
       if @stock  
         respond_to do |format|
           format.turbo_stream {
-            render turbo_stream: turbo_stream.append("test", partial: "users/result")
+            render turbo_stream: turbo_stream.update("test", partial: "users/result")
           }
         end
       else  
-        flash[:alert] = "Please enter a valid symbol to search"
-        redirect_to my_portfolio_path
+        respond_to do |format|
+          format.turbo_stream {
+            flash.now[:alert] = "Please enter a valid symbol to search"
+            render turbo_stream: turbo_stream.update("test", partial: "users/result")            
+          }
+        end
       end
     else
-      # do something different
-      flash[:alert] = "can not be empty"
-      redirect_to my_portfolio_path 
+      respond_to do |format| 
+        format.turbo_stream {
+          flash.now[:alert] = "Can not be empty"
+          render turbo_stream: turbo_stream.update("test", partial: "users/result")
+        }
+      end
     end   
   end
 end
