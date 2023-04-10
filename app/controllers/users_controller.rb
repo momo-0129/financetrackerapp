f05affe5c0e52d3d3b6c95dcaeb6250265f7b5cf
momo-0129 <tonyfang8733@gmail.com>
2023-04-10 17:03:16 +0900

    Add turbo stream render search friends result

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,30 @@ class UsersController < ApplicationController
     @friends = current_user.friends
   end
 
-  def search 
-    render json: params[:friend]
+  def search
+    if params[:friend].present? 
+      @friend = params[:friend]
+      if @friend  
+        respond_to do |format|
+          format.turbo_stream {
+            render turbo_stream: turbo_stream.update("friendslist", partial: "users/friend_result")
+          }
+        end
+      else  
+        respond_to do |format|
+          format.turbo_stream {
+            flash.now[:alert] = "Couldn't find user"
+            render turbo_stream: turbo_stream.update("friendslist", partial: "layouts/messages")            
+          }
+        end
+      end
+    else
+      respond_to do |format| 
+        format.turbo_stream {
+          flash.now[:alert] = "Can not be empty"
+          render turbo_stream: turbo_stream.update("friendslist", partial: "layouts/messages")
+        }
+      end
+    end
   end
-
 end
